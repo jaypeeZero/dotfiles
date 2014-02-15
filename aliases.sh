207587c4f950b5cc158947b876cd06c92e548489
Keith Bertelsen <keith@keithbertelsen.com>
2014-02-15 10:47:21 -0600

    Getting the code in git

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+alias ls='ls -GFh'
+alias ll='ls -al'
+alias lla='ls -al'
+
+# Things for git
+alias gs='git status'
+alias gd='git diff'
+alias gitk='open -a SourceTree'
+alias sourcetree='open -a SourceTree'
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/Dropbox/Customization/Shell
+#cd ${0%/*}
+
+. environment.sh
+. aliases.sh
+. functions.sh
+. prompt.sh
+
+cd ~
