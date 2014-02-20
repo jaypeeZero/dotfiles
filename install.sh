0420344e2ba1a04515fa50fcb22edb2a5e150358
Keith Bertelsen <keith@keithbertelsen.com>
2014-02-19 20:54:31 -0600

    Created install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# A better way of calling ln
+link_file() {
+	source="$1"
+	destination="$2"
+	
+	dest_dir="$(dirname "$destination")"
+	mkdir -p $dest_dir
+	
+	stamp=dotfiles
+	now=$(date '+%Y-%m-%d_%H-%M-%S')
+	
+	[[ -h "$destination" ]] && rm "$destination"
+	[[ -e "$destination" ]] && mv "$destination" "$destination.$stamp-$now"
+	ln -sv "$source" "$destination"
+}
+
+bin_dir="$(cd "$(dirname "$0")" && pwd)"
+
+dotfiles="
+aliases
+bash_profile
+environment
+functions
+git-prompt
+prompt
+"
+
+for source in $dotfiles
+do
+	destination="$HOME/.${source}"
+	link_file "$bin_dir/$source.sh" "$destination"
+done
