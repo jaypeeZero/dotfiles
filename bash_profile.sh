57f76beec891632b46586beb6485c5eece4101bd
Keith Bertelsen <keith@keithbertelsen.com>
2014-02-19 22:27:48 -0600

    Helps if I include my old .bash_profile

---
+++ bash_profile.sh
@@ -0,0 +1,13 @@
+. ~/Dropbox/Customization/Shell/setup.sh
+
+
+##
+# Your previous /Users/Keith/.bash_profile file was backed up as /Users/Keith/.bash_profile.macports-saved_2013-07-27_at_10:33:19
+##
+
+# MacPorts Installer addition on 2013-07-27_at_10:33:19: adding an appropriate PATH variable for use with MacPorts.
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+# Finished adapting your PATH environment variable for use with MacPorts.
+
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
