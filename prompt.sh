ca85576680708d5c942b2f5949b3d37854831d30
Keith Bertelsen <keith@keithbertelsen.com>
2014-02-20 21:55:56 -0600

    Created variables for colors, and refactored prompt to match

--- prompt.sh
+++ prompt.sh
@@ -1,8 +1,7 @@
-. git-prompt.sh
 
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWCOLORHINTS=1
 GIT_PS1_SHOWUNTRACKEDFILES=1
 GIT_PS1_SHOWSTASHSTATE=1
 
-export PS1='\[\033[36m\]\u\[\033[m\] @ \[\033[32m\]\h \[\033[33;0m\]:\[\033[0;31m\]$(__git_ps1 " %s $(get_sha) \[\033[33;0m\]:") \[\033[33;1m\]\w\[\033[m\]\n> '
+export PS1="${Cyan}\u${Color_Off} @ ${Green}\h ${Color_Off}:${Red}\$(__git_ps1 \" %s \$(get_sha) ${Color_Off}:\") ${BYellow}\w${Color_Off}\n> "
