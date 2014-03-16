bb86ff7bdfcfd4223f177cd07bf3e772bfa89f6d
Keith Bertelsen <keith@keithbertelsen.com>
2014-03-16 16:57:45 -0500

    Matching up Linux colors with my OS X colors (which are default)

--- aliases.sh
+++ aliases.sh
@@ -2,8 +2,10 @@
 # From https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
 if ls --color > /dev/null 2>&1; then # GNU `ls`
 	colorflag="--color"
+	export LS_COLORS="di=34;48:ln=35;48:so=32;48:pi=33;48:ex=31;48:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
 else # OS X `ls`
 	colorflag="-G"
+	export LSCOLORS=exfxcxdxbxegedabagacad
 fi
 
 alias ls='ls -Fh ${colorflag}'
