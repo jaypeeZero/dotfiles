9fc830eaf6780eebc6a6b2401f14f184716bd3c2
Keith Bertelsen <keith@keithbertelsen.com>
2014-03-16 12:50:08 -0500

    Added color capabilities for GNU ls

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,12 @@
-alias ls='ls -GFh'
+# Detect which `ls` flavor is in use
+# From https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+else # OS X `ls`
+	colorflag="-G"
+fi
+
+alias ls='ls -Fh ${colorflag}'
 alias ll='ls -al'
 alias lla='ls -al'
 
