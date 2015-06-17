467ced81974e6edc84fe8475f2593038752fcac8
Keith Bertelsen <keith@keithbertelsen.com>
2015-06-17 17:43:26 -0500

    Remove old MacPorts information

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,18 +1,5 @@
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
-
 . ~/.environment
 
-##
-# Your previous /Users/Keith/.bash_profile file was backed up as /Users/Keith/.bash_profile.macports-saved_2013-07-27_at_10:33:19
-##
-
-# MacPorts Installer addition on 2013-07-27_at_10:33:19: adding an appropriate PATH variable for use with MacPorts.
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-# Finished adapting your PATH environment variable for use with MacPorts.
-
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 [[ -s "$HOME/.local.bash_profile" ]] && source "$HOME/.local.bash_profile"
