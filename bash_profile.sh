da104f91dc3b324d313e5b243231f794300ac0c6
Keith Bertelsen <keith@keithbertelsen.com>
2016-01-30 12:47:07 -0600

    Update bash profile

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,5 +1,15 @@
-. ~/.environment
+#!/bin/bash
+#
+# This file is only executed for login shells. That means it gets executed when logging in over SSH or logging into KDE, but not when a new OS X terminal window is opened
 
-export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/sbin:$PATH
+# All of my settings are in bashrc, so just use that
 
-[[ -s "$HOME/.local.bash_profile" ]] && source "$HOME/.local.bash_profile"
+if [ -f ~/.bashrc ]; then
+  source ~/.bashrc
+fi
+
+#export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/sbin:$PATH
+
+#[[ -s "$HOME/.local.bash_profile" ]] && source "$HOME/.local.bash_profile"
+
+#[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
