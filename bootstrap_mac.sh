c359784b28d7444b27ff63e85d6385c3eef3a71e
Keith Bertelsen <keith@keithbertelsen.com>
2017-03-24 13:35:12 -0500

    Make boostrap less destructive for now

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -8,21 +8,21 @@ echo -e "
 ##################################
 "
 
-eval "$(ssh-agent -s)"
-ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
-echo -e "Paste the following into Github's allowed public keys to connect
----
-"
-cat ~/.ssh/id_rsa.pub
-echo -e "
----
-"
+# eval "$(ssh-agent -s)"
+# ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa
+# ssh-add ~/.ssh/id_rsa
+# echo -e "Paste the following into Github's allowed public keys to connect
+# ---
+# "
+# cat ~/.ssh/id_rsa.pub
+# echo -e "
+# ---
+# "
 
-read -p "Press ENTER when done." trash
-unset trash
-echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
-" > ~/.ssh/known_hosts
+# read -p "Press ENTER when done." trash
+# unset trash
+# echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+# " > ~/.ssh/known_hosts
 
 
 echo -e "
@@ -31,7 +31,7 @@ echo -e "
 ########################
 "
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
 echo -e "
@@ -40,6 +40,6 @@ echo -e "
 ###################################
 "
 
-git clone git@github.com:kdbertel/dotfiles.git ~/.df
+# git clone git@github.com:kdbertel/dotfiles.git ~/.df
 
 
