06a9271f2af461014542af81f711845c1f06f033
Keith Bertelsen <keith@keithbertelsen.com>
2017-03-29 16:39:44 -0500

    Update bootstrap to not error when things already exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,22 +7,25 @@ echo -e "
 ###     SSH KEY GENERATION     ###
 ##################################
 "
+if [ ! -f ~/.ssh/id_rsa ]; then
+    eval "$(ssh-agent -s)"
+    ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa
+    ssh-add ~/.ssh/id_rsa
+    echo -e "Paste the following into Github's allowed public keys to connect
+    ---
+    "
+    cat ~/.ssh/id_rsa.pub
+    echo -e "
+    ---
+    "
 
-# eval "$(ssh-agent -s)"
-# ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa
-# ssh-add ~/.ssh/id_rsa
-# echo -e "Paste the following into Github's allowed public keys to connect
-# ---
-# "
-# cat ~/.ssh/id_rsa.pub
-# echo -e "
-# ---
-# "
-
-# read -p "Press ENTER when done." trash
-# unset trash
-# echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
-# " > ~/.ssh/known_hosts
+    read -p "Press ENTER when done." trash
+    unset trash
+    echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+    " > ~/.ssh/known_hosts
+else
+    echo -e "SSH Key already generated"
+fi
 
 
 echo -e "
@@ -31,7 +34,11 @@ echo -e "
 ########################
 "
 
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ ! -f "$(which brew)" ]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo -e "Homebrew already installed"
+fi
 
 
 echo -e "
@@ -40,6 +47,9 @@ echo -e "
 ###################################
 "
 
-# git clone git@github.com:kdbertel/dotfiles.git ~/.df
-
+if [ ! -d ~/.df ]; then
+    git clone git@github.com:kdbertel/dotfiles.git ~/.df
+else
+    echo "Dotfiles already cloned"
+fi
 
