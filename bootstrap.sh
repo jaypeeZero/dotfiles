4caf42489086c023374dd875053095d18dcc6e75
Keith Bertelsen <keith@keithbertelsen.com>
2017-03-29 21:28:11 -0500

    Use better script for installing terminal theme, which should make updates better if Terminal works like expected

--- bootstrap.sh
+++ bootstrap.sh
@@ -108,11 +108,38 @@ echo -e "
 ###     INSTALL IRIDIUM TERMINAL THEME     ###
 ##############################################
 "
-if $(/usr/libexec/PlistBuddy -c "print 'Window Settings':Iridium" ~/Library/Preferences/com.apple.Terminal.plist >/dev/null 2>&1); then
-    echo "Iridium already installed as Terminal theme"
-else
-    open ~/.df/files/Iridium.terminal
-    osascript -e 'tell application "Terminal" to do script "exit" in window 1' >/dev/null 2>&1
-    echo "Iridium installed as Terminal theme"
-fi
+# Found at https://github.com/geerlingguy/mac-dev-playbook/issues/26
+osascript <<EOD
+tell application "Terminal"
+	local allOpenedWindows
+	local initialOpenedWindows
+	local windowID
+	set themeName to "Iridium" (* Set the name of the theme*)
+	(* Store the IDs of all the open terminal windows. *)
+	set initialOpenedWindows to id of every window
+	(* Open the custom theme so that it gets added to the list
+	   of available terminal themes (note: this will open two
+	   additional terminal windows). *)
+	do shell script "open '$HOME/.df/files/" & themeName & ".terminal'"
+	(* Wait a little bit to ensure that the custom theme is added. *)
+	delay 1
+	(* Set the custom theme as the default terminal theme. *)
+	set default settings to settings set themeName
+	(* Get the IDs of all the currently opened terminal windows. *)
+	set allOpenedWindows to id of every window
+	repeat with windowID in allOpenedWindows
+		(* Close the additional windows that were opened in order
+		   to add the custom theme to the list of terminal themes. *)
+		if initialOpenedWindows does not contain windowID then
+			close (every window whose id is windowID)
+		(* Change the theme for the initial opened terminal windows
+		   to remove the need to close them in order for the custom
+		   theme to be applied. *)
+		else
+			set current settings of tabs of (every window whose id is windowID) to settings set themeName
+		end if
+	end repeat
+end tell
+EOD
+
 
