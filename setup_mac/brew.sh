c487a9f81f528815ad9ff0bc5438cf0afafad409
Keith Bertelsen <keith@keithbertelsen.com>
2017-03-31 16:24:11 -0500

    I think I'm good on brews for now. If I need them again, I can always re-add them.

--- setup_mac/brew.sh
+++ setup_mac/brew.sh
@@ -2,33 +2,6 @@
 #
 # Brew all the things
 
-##################
-# Things to brew #
-##################
-
-binaries=(
-#    bash
-    carthage
-#    chruby
-#    cloc
-#    coreutils
-    dockutil
-    ffmpeg
-#    git
-    git-annex
-    gs
-#    mas
-#    multimarkdown
-#    python
-#    reattach-to-user-namespace
-#    ruby-install
-#    tig
-#    tmux
-#    tree
-#    vim
-    webfsd
-)
-
 casks=(
 #    appcleaner
 #    atom
